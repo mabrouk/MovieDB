d0e85b1f6782cc138dbad05abc6a920f01e64930
Ahmad Mabrouk <mabrouk.ee@gmail.com>
2016-11-23 13:27:20 +0200

    Use NavigationView instead of ListView in DrawerLayout

--- app/src/main/java/com/mabrouk/moviedb/MainActivity.java
+++ app/src/main/java/com/mabrouk/moviedb/MainActivity.java
@@ -1,19 +1,14 @@
 package com.mabrouk.moviedb;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
-    ListView drawerList;
+    NavigationView navigationView;
     String[] sections;
 
     MainSectionsFactory factory = new MainSectionsFactory();
@@ -24,59 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         sections = getResources().getStringArray(R.array.drawer_list);
-
-        drawerList = (ListView) findViewById(R.id.drawer_list);
-        drawerList.setAdapter(new DrawerListAdapter(sections));
-        drawerList.setOnItemClickListener((parent, view, position, id) -> selectSection(position));
-
-        selectSection(0);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this::selectSection);
+        selectSection(navigationView.getMenu().findItem(R.id.movies));
     }
 
-    private void selectSection(int position) {
+    boolean selectSection(MenuItem item) {
+        int position = 0;
+        switch (item.getItemId()) {
+            case R.id.movies:
+                position = 0;
+                break;
+            case R.id.tv:
+                position = 1;
+                break;
+            case R.id.people:
+                position = 2;
+                break;
+        }
         setTitle(sections[position]);
-        drawerLayout.closeDrawer(drawerList);
+        drawerLayout.closeDrawers();
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content_frame, factory.fragmentForPage(position))
                 .commit();
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
-            getSupportFragmentManager().popBackStack();
-        }else {
-            super.onBackPressed();
-        }
-    }
-
-    static class DrawerListAdapter extends BaseAdapter {
-        String[] sections;
-        public DrawerListAdapter(String[] sections) {
-            this.sections = sections;
-        }
-
-        @Override
-        public int getCount() {
-            return sections.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return sections[position];
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @SuppressLint("ViewHolder")
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            convertView = inflater.inflate(R.layout.row_drawer_list, parent, false);
-            ((TextView) convertView.findViewById(R.id.section)).setText(sections[position]);
-            return convertView;
-        }
+        item.setChecked(true);
+        return true;
     }
 }
